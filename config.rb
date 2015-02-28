af91ee9ee11b181ee2cbdcb94dac0e7ddccc67bd
Damien Bachet <dbachet@gmail.com>
2015-02-28 10:16:15 +0100

    Add twitter config

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'dotenv'
+Dotenv.load
+
+require "twitter"
+
+@twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
